8507ee7cc39dbf5b7087559de3478208581e4ae1
Ayush Kaushal <bibhuayush@gmail.com>
2022-07-22 20:44:54 +0530

    Django key secured

--- BriefIt_Summarizer/settings.py
+++ BriefIt_Summarizer/settings.py
@@ -14,6 +14,8 @@ import os
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6@*6qpw0vj6kpw9$a=x68&2te9q@y8)wgod8(1eq^ge&u7!6n5'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
